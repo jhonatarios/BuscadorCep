757663d389d06db7b7063547d7382f9fb9c762e3
Jhonata Rios <72048481+jhonatarios@users.noreply.github.com>
2023-08-31 21:28:33 -0300

    BuscadorCep
    Aplicativo de Busca de CEP em React Native

--- App.js
+++ App.js
@@ -1,20 +1,189 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react'
+import { View, StyleSheet, Text, TextInput, TouchableOpacity, Alert } from 'react-native'
+import MaskInput from 'react-native-mask-input';
+import Api from './src/services/api'
 
 export default function App() {
+
+  const [cep, setCep] = useState('')
+  const [logradouro, setLogradouro] = useState('')
+  const [bairro, setBairro] = useState('')
+  const [localidade, setLocalidade] = useState('')
+  const [uf, setUF] = useState('')
+  const [ddd, setDDD] = useState('')
+
+  async function searchCep() {
+    if (cep == '') {
+      Alert.alert('CEP inválido!')
+      setCep('')
+    }
+
+    try {
+      const response = await Api.get(`/${cep}/json/`)
+      setLogradouro(response.data.logradouro)
+      setBairro(response.data.bairro)
+      setLocalidade(response.data.localidade)
+      setUF(response.data.uf)
+      setDDD(response.data.ddd)
+    } catch (error) {
+      console.log('ERROR: ' + error)
+      Alert.alert('CEP inválido!')
+      setCep('')
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      <View style={styles.topBar}>
+        <Text style={styles.topBarTitle}>Buscador de CEP</Text>
+      </View>
+
+      <View style={styles.appContainer}>
+        <Text style={styles.appDescription}>
+          Um CEP no formato de 8 dígitos (somente números) deve ser fornecido, após
+          será fornecido o logradouro, bairro, cidade, estado e o
+          DDD correspondente.
+        </Text>
+        <View style={styles.containerSearchCep}>
+          <MaskInput
+            value={cep}
+            onChangeText={(cepText) => setCep(cepText)}
+            placeholder="CEP"
+            mask={[/\d/, /\d/, /\d/, /\d/, /\d/, "-", /\d/, /\d/, /\d/]}
+            style={styles.inputSearchCep}
+            keyboardType="numeric"
+            placeholderTextColor="#fff"
+          />
+          <TouchableOpacity style={styles.searchButton} onPress={searchCep}>
+            <Text style={styles.searchButtonText}>Buscar</Text>
+          </TouchableOpacity>
+        </View>
+
+        <TextInput
+          value={logradouro}
+          placeholder="Logradouro"
+          style={styles.textBox}
+          placeholderTextColor="#fff"
+        />
+
+        <TextInput
+          value={bairro}
+          placeholder="Bairro"
+          style={styles.textBox}
+          placeholderTextColor="#fff"
+        />
+
+        <TextInput
+          value={localidade}
+          placeholder="Cidade"
+          style={styles.textBox}
+          placeholderTextColor="#fff"
+        />
+
+        <View style={styles.containerBottom}>
+          <TextInput
+            value={uf}
+            placeholder="UF"
+            style={styles.textBoxBottom}
+            placeholderTextColor="#fff"
+          />
+          <TextInput
+            value={ddd}
+            placeholder="DDD"
+            style={styles.textBoxBottom}
+            placeholderTextColor="#fff"
+          />
+        </View>
+      </View>
+
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: 'column',
+    backgroundColor: '#111'
   },
-});
+  appContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  topBar: {
+    flexDirection: 'row',
+    height: 70,
+    backgroundColor: '#19ff57',
+    justifyContent: 'center'
+  },
+  topBarTitle: {
+    color: '#111',
+    fontSize: 24,
+    fontWeight: 'bold',
+    padding: 16,
+    alignSelf: 'center'
+  },
+  appDescription: {
+    color: '#d1d1d1',
+    padding: 16,
+    marginTop: 10,
+    fontSize: 16
+  },
+  containerSearchCep: {
+    flexDirection: 'row',
+    height: 80,
+    width: 380,
+    marginHorizontal: 16,
+    justifyContent: 'space-between'
+  },
+  inputSearchCep: {
+    borderColor: '#333',
+    borderWidth: 1,
+    width: 200,
+    fontSize: 24,
+    marginTop: 20,
+    marginEnd: 8,
+    padding: 8,
+    justifyContent: 'center',
+    alignItems: 'center',
+    color: '#fff',
+  },
+  searchButton: {
+    backgroundColor: '#19ff57',
+    width: 172,
+    height: 60,
+    marginTop: 20,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  searchButtonText: {
+    color: '#111',
+    fontWeight: 'bold',
+    fontSize: 18,
+  },
+  textBox: {
+    borderColor: '#333',
+    borderWidth: 1,
+    width: 380,
+    fontSize: 18,
+    marginHorizontal: 16,
+    marginTop: 10,
+    padding: 8,
+    color: '#fff',
+  },
+  containerBottom: {
+    flexDirection: 'row',
+    width: 380,
+    justifyContent: 'space-between'
+  },
+  textBoxBottom: {
+    borderColor: '#333',
+    borderWidth: 1,
+    width: '49%',
+    fontSize: 18,
+    marginTop: 10,
+    padding: 8,
+    color: '#fff'
+  }
+})
--- README.md
+++ README.md
@@ -1,2 +1,72 @@
-# BuscadorCep
- 
+# BuscadorCep - Aplicativo de Busca de CEP em React Native
+
+O BuscadorCep é um aplicativo simples desenvolvido em React Native que permite aos usuários buscar informações de endereços a partir de um CEP fornecido. Ele utiliza a API ViaCEP para obter dados de endereços com base no CEP digitado pelo usuário, e utiliza a biblioteca Axios para fazer as requisições à API. Além disso, o aplicativo utiliza a biblioteca react-native-mask-input para criar uma máscara no campo de entrada de CEP, garantindo uma melhor experiência do usuário.
+
+## Índice
+
+- [Funcionalidades](#funcionalidades)
+- [Instalação](#instalação)
+- [Licença](#licença)
+
+## Funcionalidades
+
+- Busca de Endereço por CEP: O aplicativo permite ao usuário inserir um CEP e, ao pressionar um botão de busca, recupera informações de endereço correspondentes a esse CEP, incluindo logradouro, bairro, cidade, estado e DDD.
+
+- Máscara de Entrada de CEP: A biblioteca react-native-mask-input é utilizada para formatar automaticamente a entrada do CEP, adicionando os caracteres de formatação corretos (por exemplo, "12345-678") à medida que o usuário digita.
+
+
+### Pré-requisitos
+
+Certifique-se de que você tenha as seguintes ferramentas instaladas em sua máquina:
+
+- [Node.js](https://nodejs.org/) (v14 ou superior)
+- [Expo CLI](https://docs.expo.dev/get-started/installation/) (v4 ou superior) - você pode instalá-lo globalmente usando o seguinte comando:
+
+  ```bash
+  npm install -g expo-cli
+  ```
+
+### Passo 1: Clone o repositório
+
+    git clone https://github.com/jhonatarios/BuscadorCep
+    cd BuscadorCep
+
+### Passo 2: Instale as dependências
+
+    npm install
+
+### Passo 3: Configuração do ambiente
+
+Certifique-se de ter um emulador Android/iOS instalado ou o aplicativo Expo Go instalado em um dispositivo físico. Você pode baixar o Expo Go na Google Play Store ou na Apple App Store.
+
+### Passo 4: Inicie o aplicativo
+
+    expo start
+
+Isso abrirá o Expo Developer Tools em seu navegador. Você terá a opção de executar o aplicativo em um emulador ou em um dispositivo físico.
+
+- Para executar o aplicativo em um emulador Android/iOS, clique no link apropriado na página do Expo Developer Tools.
+- Para executar o aplicativo em um dispositivo físico, abra o aplicativo Expo Go em seu dispositivo e escaneie o QR code exibido na página do Expo Developer Tools.
+
+O aplicativo BuscadorCep agora deverá ser aberto no emulador/dispositivo.
+
+### Dicas adicionais
+
+- Caso você esteja enfrentando problemas de instalação ou execução, verifique a documentação oficial do Expo para soluções de problemas comuns.
+
+- Para personalizar o aplicativo com suas próprias informações, verifique os arquivos relevantes no código-fonte.
+
+- Divirta-se explorando o BuscadorCep!
+
+## Configuração da API ViaCEP
+
+O aplicativo utiliza a API gratuita ViaCEP (https://viacep.com.br/) para obter informações de endereço. Certifique-se de que você possui acesso à internet para que o aplicativo possa fazer as requisições corretamente.
+Bibliotecas Utilizadas
+
+- Axios: Biblioteca para fazer requisições HTTP. Utilizada para se comunicar com a API ViaCEP.
+
+- react-native-mask-input: Biblioteca para criação de máscaras em campos de entrada. Utilizada para formatar o campo de CEP.
+
+## Licença
+
+Este projeto está licenciado sob a [Creative Commons Attribution-NonCommercial 4.0 International License](https://creativecommons.org/licenses/by-nc/4.0/).
